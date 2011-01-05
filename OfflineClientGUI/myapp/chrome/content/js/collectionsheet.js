779459518b9b9f967b257cec0ad9726ecace690c
Vishwas Babu A J <vishwas@confluxtechnologies.com>
2011-01-05 12:28:49 +0530

    Merging wrong "windows" commit
    http://support.github.com/discussions/repos/1187-git-remove-rm-folder-is-not-working-for-me

---
+++ OfflineClientGUI/myapp/chrome/content/js/collectionsheet.js
@@ -0,0 +1,102 @@
+(function( $ ){
+  $.fn.myPlugin = function() {
+
+// alert($(this).get(0)); 
+
+ var treechildren = $(this).get(0);
+ 
+//var treechildren = document.getElementById("firstchild");
+
+var synchedData = '{"collectionsheet":{"center":[{"name":"C1","group":[{"name":"G11","member":[{"name":"mem111"},{"name":"mem112"}]},{"name":"G12","member":[{"name":"mem121"},{"name":"mem122"}]}]},{"name":"C2","group":[{"name":"G21","member":[{"name":"mem211"},{"name":"mem212"}]},{"name":"G22","member":[{"name":"mem221"},{"name":"mem222"}]}]}]}}';
+var obj = jQuery.parseJSON(synchedData);
+
+$.each(obj.collectionsheet.center, function(i, centervalues) {           //looping group
+		
+		var treeitem = document.createElement("treeitem");
+		treeitem.setAttribute("container","true");
+		treeitem.setAttribute("open","true");
+		
+		
+		var treerow = document.createElement("treerow");
+		var treecell = document.createElement("treecell");
+		treecell.setAttribute("label",centervalues.name);
+		treerow.appendChild(treecell);
+		treeitem.appendChild(treerow);
+		
+		var centertreechildren = document.createElement("treechildren");
+		//alert( 'Center Name : ' + centervalues.name);
+	
+	$.each(centervalues.group, function(j, groupvalues) {
+			//alert('Group Name: ' + groupvalues.name);
+			var grouptreeitem = document.createElement("treeitem");
+			grouptreeitem.setAttribute("container","true");
+			grouptreeitem.setAttribute("open","true");		
+			var treerow = document.createElement("treerow");
+			var treecell = document.createElement("treecell");
+			treecell.setAttribute("label",groupvalues.name);
+			treerow.appendChild(treecell);
+			grouptreeitem.appendChild(treerow);
+			
+			
+			var grouptreechildren = document.createElement("treechildren");
+				
+			$.each(groupvalues.member, function(j, membervalues) {
+					var membertreeitem = document.createElement("treeitem");
+					membertreeitem.setAttribute("container","true");
+					membertreeitem.setAttribute("open","true");		
+					var treerow = document.createElement("treerow");
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label",membervalues.name);
+					treecell.setAttribute("editable","false");
+					treerow.appendChild(treecell);
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label",membervalues.name);
+					treecell.setAttribute("editable","false");
+					treerow.appendChild(treecell);
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label",membervalues.name);
+					treecell.setAttribute("editable","false");
+					treerow.appendChild(treecell);
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label","00.0");
+					treerow.appendChild(treecell);
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label","230.0");
+					treerow.appendChild(treecell);
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label","P");
+					treerow.appendChild(treecell);
+					
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("value","true");
+					treecell.setAttribute("editable","true");
+					treecell.setAttribute("onclick","alert('bbb');");
+					
+					treerow.appendChild(treecell);
+					
+					
+					
+					membertreeitem.appendChild(treerow);
+					
+					grouptreechildren.appendChild(membertreeitem);
+					
+					});	
+
+			grouptreeitem.appendChild(grouptreechildren);
+			centertreechildren.appendChild(grouptreeitem);	
+			treeitem.appendChild(centertreechildren);
+	});	
+		treechildren.appendChild(treeitem);
+});	
+
+ 
+	return this;
+  };
+})( jQuery );
---
+++ OfflineClientGUI/myapp/chrome/content/js/main.js
@@ -0,0 +1,84 @@
+function parseXml(a)
+{
+
+var treechildren = document.getElementById("firstchild");
+
+var synchedData = '{"collectionsheet":{"center":[{"name":"C1","group":[{"name":"G11","member":[{"name":"mem111"},{"name":"mem112"}]},{"name":"G12","member":[{"name":"mem121"},{"name":"mem122"}]}]},{"name":"C2","group":[{"name":"G21","member":[{"name":"mem211"},{"name":"mem212"}]},{"name":"G22","member":[{"name":"mem221"},{"name":"mem222"}]}]}]}}';
+var obj = jQuery.parseJSON(synchedData);
+
+$.each(obj.collectionsheet.center, function(i, centervalues) {           //looping group
+		
+		var treeitem = document.createElement("treeitem");
+		treeitem.setAttribute("container","true");
+		treeitem.setAttribute("open","true");
+		
+		
+		var treerow = document.createElement("treerow");
+		var treecell = document.createElement("treecell");
+		treecell.setAttribute("label",centervalues.name);
+		treerow.appendChild(treecell);
+		treeitem.appendChild(treerow);
+		
+		var centertreechildren = document.createElement("treechildren");
+		//alert( 'Center Name : ' + centervalues.name);
+	
+	$.each(centervalues.group, function(j, groupvalues) {
+			//alert('Group Name: ' + groupvalues.name);
+			var grouptreeitem = document.createElement("treeitem");
+			grouptreeitem.setAttribute("container","true");
+			grouptreeitem.setAttribute("open","true");		
+			var treerow = document.createElement("treerow");
+			var treecell = document.createElement("treecell");
+			treecell.setAttribute("label",groupvalues.name);
+			treerow.appendChild(treecell);
+			grouptreeitem.appendChild(treerow);
+			
+			
+			var grouptreechildren = document.createElement("treechildren");
+				
+			$.each(groupvalues.member, function(j, membervalues) {
+					var membertreeitem = document.createElement("treeitem");
+					membertreeitem.setAttribute("container","true");
+					membertreeitem.setAttribute("open","true");		
+					var treerow = document.createElement("treerow");
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label",membervalues.name);
+					treecell.setAttribute("editable","false");
+					treerow.appendChild(treecell);
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label",membervalues.name);
+					treecell.setAttribute("editable","false");
+					treerow.appendChild(treecell);
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label",membervalues.name);
+					treecell.setAttribute("editable","false");
+					treerow.appendChild(treecell);
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label","00.0");
+					treerow.appendChild(treecell);
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label","230.0");
+					treerow.appendChild(treecell);
+					
+					var treecell = document.createElement("treecell");
+					treecell.setAttribute("label","P");
+					treerow.appendChild(treecell);
+					
+					membertreeitem.appendChild(treerow);
+					
+					grouptreechildren.appendChild(membertreeitem);
+					
+					});	
+
+			grouptreeitem.appendChild(grouptreechildren);
+			centertreechildren.appendChild(grouptreeitem);	
+			treeitem.appendChild(centertreechildren);
+	});	
+		treechildren.appendChild(treeitem);
+});	
+}
