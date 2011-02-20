9ab8ca3c465778947584746c5fa271dba5b8515d
Vishwas Babu A J <vishwas@confluxtechnologies.com>
2011-02-20 12:05:52 +0530

    storing latest working copy

---
+++ flamingo/chrome/content/js/global.js
@@ -0,0 +1,15 @@
+var EXPORTED_SYMBOLS = ["Ctx", "authenticationUtils", "baseXUtils", "dummy"];
+
+/**java Objects connected using Live Connect**/
+var authenticationUtils;
+var baseXUtils;
+
+
+var Ctx = {
+    user: "",
+    password: "",
+    token: "",
+    role: "",
+};
+
+var dummy = "dummy";
